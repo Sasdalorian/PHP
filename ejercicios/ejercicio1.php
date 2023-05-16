63c92ac014b93294edf3a0f3449da74197d71d89
Sasdalorian <randomweas1100@gmail.com>
2023-05-16 18:44:01 -0400

    Empezando PHP

---
+++ ejercicios/ejercicio1.php
@@ -0,0 +1,11 @@
+<?php 
+    // Comentario en una linea
+    /*
+        Comentario en mas de una linea
+    */
+
+    // Formas de imprimir
+    echo "Hola, soy un echo imprimiendo en html!";
+    print_r("Hola! soy un print_r, puedo imprimir de todo tipo de datos");
+    print("Hola! soy un simple print")
+?>
